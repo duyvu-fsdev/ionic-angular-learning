b9c3ac4736e4d8595259e1612482b979260ab899
duyvu-fsdev <duyvu.fullstackdev@gmail.com>
2024-10-03 00:56:35 +0700

    II.III - Lesson 3: Create the application’s HousingLocation component

--- angular18-base/src/app/app.module.ts
+++ angular18-base/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { HousingLocationComponent } from './components/housing-location/housing-location.component';
 
 @NgModule({
- declarations: [AppComponent, HomeComponent],
+ declarations: [AppComponent, HomeComponent, HousingLocationComponent],
  imports: [BrowserModule, AppRoutingModule],
  providers: [],
  bootstrap: [AppComponent],
