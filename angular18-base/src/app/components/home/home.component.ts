b43377c1c11b800bbe74cd4e37b0de2fc7dabf6e
duyvu-fsdev <duyvu.fullstackdev@gmail.com>
2024-10-03 10:33:33 +0700

    II.IV - lesson 4 - Creating an interface

--- angular18-base/src/app/components/home/home.component.ts
+++ angular18-base/src/app/components/home/home.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from '../../interfaces/housinglocation';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+ selector: 'app-home',
+ templateUrl: './home.component.html',
+ styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+ readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+ housingLocation: HousingLocation = {
+  id: 9999,
+  name: 'Test Home',
+  city: 'Test city',
+  state: 'ST',
+  photo: `${this.baseUrl}/example-house.jpg`,
+  availableUnits: 99,
+  wifi: true,
+  laundry: false,
+ };
 }
