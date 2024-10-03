7025a3b0a5ccc49e055396220b9056e114a23039
duyvu-fsdev <duyvu.fullstackdev@gmail.com>
2024-10-03 16:40:53 +0700

    II.XIII - Lesson 13. Add search feature (Home)

--- angular18-base/src/app/components/home/home.component.ts
+++ angular18-base/src/app/components/home/home.component.ts
@@ -10,7 +10,21 @@ import { HousingService } from '../../services/housing.service';
 export class HomeComponent {
  housingLocationList: HousingLocation[] = [];
  housingService: HousingService = inject(HousingService);
+ filteredLocationList: HousingLocation[] = [];
+
  constructor() {
   this.housingLocationList = this.housingService.getAllHousingLocations();
+  this.filteredLocationList = this.housingLocationList;
+ }
+
+ filterResults(text: string) {
+  if (!text) {
+   this.filteredLocationList = this.housingLocationList;
+   return;
+  }
+
+  this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+   housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+  );
  }
 }
