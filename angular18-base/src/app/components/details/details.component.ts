2276cf4790e31955d59bfdaf3c3d5eece11f9483
duyvu-fsdev <duyvu.fullstackdev@gmail.com>
2024-10-03 13:40:48 +0700

    II.XI - Lesson 11. Integrate details page into application

--- angular18-base/src/app/components/details/details.component.ts
+++ angular18-base/src/app/components/details/details.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../../services/housing.service';
+import { HousingLocation } from '../../interfaces/housinglocation';
 
 @Component({
-  selector: 'app-details',
-  templateUrl: './details.component.html',
-  styleUrl: './details.component.scss'
+ selector: 'app-details',
+ templateUrl: './details.component.html',
+ styleUrl: './details.component.scss',
 })
 export class DetailsComponent {
-
+ route: ActivatedRoute = inject(ActivatedRoute);
+ housingService = inject(HousingService);
+ housingLocation: HousingLocation | undefined;
+ housingLocationId = -1;
+ constructor() {
+  const housingLocationId = Number(this.route.snapshot.params['id']);
+  this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+ }
 }
